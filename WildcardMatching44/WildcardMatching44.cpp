40713b848977c59c1512a66dc1f8e4cbc2951ad9
Sergey <mr.ultimateground@gmail.com>
2025-06-26 10:16:01 +0200

    1400 tests correct

--- WildcardMatching44/WildcardMatching44.cpp
+++ WildcardMatching44/WildcardMatching44.cpp
@@ -6,8 +6,8 @@
 using namespace std;
 
 bool isMatch(string s, string p) {
-    if (p == "*")
-        return true;
+    /*if (p == "*")
+        return true;*/
 
     auto sIter = s.begin();
     stringstream cutter(p);
@@ -20,22 +20,31 @@ bool isMatch(string s, string p) {
         auto subIter = subP.begin();
         while (subIter!=subP.end())
         {
+            if (sIter == s.end())
+                return false;
             if (*subIter==*sIter || *subIter=='?')
             {
                 subIter++;
             }
             else {
                 subIter = subP.begin();
+                if (*subIter == *sIter || *subIter == '?')
+                {
+                    subIter++;
+                }
             }
 
             sIter++;
-            if (sIter == s.end())
-                return false;
         }
 
     }
+    if (p.size()>0)
+    if (p.at(p.length() - 1)=='*')
+    {
+        return true;
+    }
 
-    return sIter==s.end();
+    return sIter==s.end()||sIter==s.begin();
 }
 
 int main(int argc, char* argv[]) {
@@ -48,4 +57,22 @@ TEST_CASE("Basic values") {
     REQUIRE(!isMatch("aa", "a"));
     REQUIRE(isMatch("aa", "*"));
     REQUIRE(!isMatch("cb", "?a"));
+}
+
+TEST_CASE("Classic values") {
+    REQUIRE(isMatch("abc", "abc"));//4
+    REQUIRE(isMatch("a123a456", "a*a*"));
+    REQUIRE(isMatch("12aabababc", "*a*ab*abc"));
+}
+TEST_CASE("Extreme values") {
+    REQUIRE(isMatch("", ""));//7
+    REQUIRE(isMatch("", "*"));
+    REQUIRE(isMatch("a", "?"));
+    REQUIRE(isMatch("a", "?*"));//10
+    REQUIRE(isMatch("", "****"));
+    REQUIRE(isMatch("a*a", "****"));
+    REQUIRE(isMatch("aba", "****"));
+}
+TEST_CASE("Leetcode values") {
+    REQUIRE(isMatch("abcabczzzde", "*abc???de*"));
 }
