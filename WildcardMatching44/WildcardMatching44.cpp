3a40d4fba1ab6c7ddc7f7deef00fab9fe577ae48
Sergey <mr.ultimateground@gmail.com>
2025-06-26 13:29:28 +0200

    new version of logic

--- WildcardMatching44/WildcardMatching44.cpp
+++ WildcardMatching44/WildcardMatching44.cpp
@@ -27,15 +27,19 @@ bool isEqual(string::iterator first,
 
 }
 
-list<string::iterator> findSubStrings(string data, string subString) {
-    if (subString.length() > data.length())
-        throw exception("bad args");
-    
+bool isEqualStrings(string first, string second) {
+    if (first.length() != second.length())
+    {
+        return false;
+    }
+    return isEqual(first.begin(), first.end(), second.begin(), second.end());
+}
+
+list<string::iterator> findSubStrings(string::iterator dataIter, string::iterator dataEnd, string subString) {
     list<string::iterator> res = {};
     size_t subLength = subString.length();
-    auto dataIter = data.begin();
-    auto dataEnd = data.end()-subLength;
-    while (dataIter!=dataEnd)
+    auto dataEndPoint = dataEnd -subLength+1;
+    while (dataIter!= dataEndPoint)
     {
         if (isEqual(dataIter,dataIter+subLength,subString.begin(), subString.end()))
         {
@@ -47,46 +51,50 @@ list<string::iterator> findSubStrings(string data, string subString) {
         
 }
 
-bool isMatch(string s, string p) {
-    /*if (p == "*")
-        return true;*/
+bool isMatchRecursive(string::iterator dataIter, string::iterator dataEnd, list<string> params) {
+    if (params.empty()) {
+        return dataIter == dataEnd;
+    }
+    auto locations = findSubStrings(dataIter, dataEnd, params.front());
+    size_t param_size = params.front().size();
+    params.pop_front();
+    bool res = false;
+    for (auto iter : locations)
+    {
+        res |= isMatchRecursive(iter+param_size,dataEnd,params);
+    }
+    return res;
+}
 
-    auto sIter = s.begin();
+bool isMatch(string s, string p) {
+  
     stringstream cutter(p);
     string subP;
+    list<string> params = {};
+    size_t params_length = 0;
+
+    if (p.find('*')==string::npos)
+    {
+        return isEqualStrings(s, p);
+    }
 
     while (!cutter.eof())
     {
         getline(cutter, subP, '*');
-
-        auto subIter = subP.begin();
-        while (subIter!=subP.end())
+        if (subP != "")
         {
-            if (sIter == s.end())
-                return false;
-            if (*subIter==*sIter || *subIter=='?')
-            {
-                subIter++;
-            }
-            else {
-                subIter = subP.begin();
-                if (*subIter == *sIter || *subIter == '?')
-                {
-                    subIter++;
-                }
-            }
-
-            sIter++;
+            params.push_back(subP);
+            params_length += subP.length();
         }
-
     }
-    if (p.size()>0)
-    if (p.at(p.length() - 1)=='*')
-    {
+
+    if (params.empty())
         return true;
-    }
+    if (params_length>s.length())
+        return false;
 
-    return sIter==s.end()||sIter==s.begin();
+
+    return isMatchRecursive(s.begin(), s.end(), params);
 }
 
 int main(int argc, char* argv[]) {
